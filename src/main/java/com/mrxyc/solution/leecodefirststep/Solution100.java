dae9a3aa8daa5dab275133d919a9b51f96fdfbcc
haohao.sun <haohao.sun@qunar.com>
2020-08-07 14:50:30 +0800

    相同树递归解法

---
+++ src/main/java/com/mrxyc/solution/leecodefirststep/Solution100.java
@@ -0,0 +1,21 @@
+package com.mrxyc.solution.leecodefirststep;
+
+import com.mrxyc.common.TreeNode;
+
+/**
+ * 相同的树
+ */
+public class Solution100 {
+    public boolean isSameTree(TreeNode p,TreeNode q){
+        if(q==null&&p==null){
+            return true;
+        }
+        if(q==null||p==null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+    }
+}
