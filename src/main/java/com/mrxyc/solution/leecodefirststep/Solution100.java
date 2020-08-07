bab2650b06d36dbe8dcdb403c7e2ec83fce0430c
haohao.sun <haohao.sun@qunar.com>
2020-08-07 14:59:33 +0800

    格式化

--- src/main/java/com/mrxyc/solution/leecodefirststep/Solution100.java
+++ src/main/java/com/mrxyc/solution/leecodefirststep/Solution100.java
@@ -6,16 +6,16 @@ import com.mrxyc.common.TreeNode;
  * 相同的树
  */
 public class Solution100 {
-    public boolean isSameTree(TreeNode p,TreeNode q){
-        if(q==null&&p==null){
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (q == null && p == null) {
             return true;
         }
-        if(q==null||p==null){
+        if (q == null || p == null) {
             return false;
         }
-        if(p.val!=q.val){
+        if (p.val != q.val) {
             return false;
         }
-        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
